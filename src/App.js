7058b9864d7091b0702b1effd0722e535207b75a
Mohamed Yehia <eng.essa85@gmail.com>
2023-12-31 21:32:37 +0200

    creating products files and adding the products images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import products from "./products";
 
 function App() {
   return (
     <div className="App">
       <h1>Thi is my web app Task1</h1>
       <p>this is the solution of the first task</p>
+      <img src="/market.jpg" alt="error" />
+      <div>
+        {products.map((element) => {
+          return (
+            <div>
+              <h1>{element.name}</h1>
+              <p>{element.price}</p>
+              <img src={element.image} alt={element.name} />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/products.js
@@ -0,0 +1,14 @@
+const products = [
+    {
+      name: 'Product A',
+      price: 25.99,
+      image: '/pro1.jpg' // Path to the image in the public folder
+    },
+    {
+      name: 'Product B',
+      price: 39.99,
+      image: '/pro2.jpg' // Path to another image in the public folder
+    }
+  ];
+  
+  export default products;
